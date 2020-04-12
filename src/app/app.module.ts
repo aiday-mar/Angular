2ef2a0ed3c8dc0c385a79b32c11e5cdcea59990f
aiday-mar <aidaymarlenkyzy@gmail.com>
2020-04-12 15:21:32 +0200

    I have added the imports necessary to connect the app to the firebase in the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,34 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+// import the firebase related packages
+
+import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJhXQWXEymyZO2tySmhqlWQnOhuBVijZ8",
+  authDomain: "appdata-67dc1.firebaseapp.com",
+  databaseURL: "https://appdata-67dc1.firebaseio.com",
+  projectId: "appdata-67dc1",
+  storageBucket: "appdata-67dc1.appspot.com",
+  messagingSenderId: "812140894208",
+  appId: "1:812140894208:web:54ecab7dfdcdae5fc560e5",
+  measurementId: "G-6PFMMHEC8H"
+};
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    ],
+
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
