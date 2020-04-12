c3726b6eef881c1eecff0d3541588916c68f17fb
aiday-mar <aidaymarlenkyzy@gmail.com>
2020-04-12 13:15:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-v1l5y7
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-v1l5y7)
---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+
+// the below is a mandatory code part of the angular framework
+export class AppComponent  {
+
+}
