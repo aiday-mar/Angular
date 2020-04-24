3b2c1ecc635d92e2f730b5ee9087b7aa0bafe521
aiday-mar <aidaymarlenkyzy@gmail.com>
2020-04-24 19:23:03 +0200

    Here we have the camera appearing and we can take some pictures in live, now I may decide to add this picture to the database maybe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { DisplayPostsHomeService } from './display-posts-home.service';
 import { Post } from './Models/post.model';
 
@@ -16,11 +16,21 @@ import { Post } from './Models/post.model';
 // the below is a mandatory code part of the angular framework
 export class AppComponent implements OnInit {
 
+  @ViewChild("video")
+  public video: ElementRef;
+
+  @ViewChild("canvas")
+  public canvas: ElementRef;
+
+  public captures: Array<any>;
+
   posts: Post[];
 
 // you initialize a service in the constructor of the component
 // https://dzone.com/articles/what-is-a-service-in-angular-js-why-to-use-it
-  constructor(private displayPostsHomeService: DisplayPostsHomeService) { }
+  constructor(private displayPostsHomeService: DisplayPostsHomeService) {
+    this.captures = [];
+  }
 
 // https://www.angularjswiki.com/angular/what-is-the-difference-between-constructor-and-ngoninit-in-angular/
   ngOnInit() {
@@ -35,5 +45,21 @@ export class AppComponent implements OnInit {
         } as Post;
       })
     });
+  }
+
+  
+  public ngAfterViewInit() {
+    if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
+            //this.video.nativeElement.src = window.URL.createObjectURL(stream);
+            this.video.nativeElement.srcObject = stream;
+            this.video.nativeElement.play();
+        });
+    }
+  }
+
+  public capture() {
+      var context = this.canvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 640, 480);
+      this.captures.push(this.canvas.nativeElement.toDataURL("image/png"));
   }
 }
